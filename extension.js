b2f42dc5c5d02295b48dc4728e35e55a77f8803b
NintySwinty <31569763+NintyS@users.noreply.github.com>
2023-12-20 19:08:39 +0100

    base for extensions

---
+++ extension.js
@@ -0,0 +1,36 @@
+const { GObject, St } = imports.gi;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+
+var HelloWorldButton = GObject.registerClass(
+class HelloWorldButton extends PanelMenu.Button {
+    _init() {
+        super._init(0.0, 'Hello World');
+
+        let icon = new St.Icon({
+            icon_name: 'system-run-symbolic',
+            style_class: 'system-status-icon'
+        });
+
+        this.add_child(icon);
+    }
+});
+
+class Extension {
+    constructor() {
+    }
+
+    enable() {
+        this._indicator = new HelloWorldButton();
+        Main.panel.addToStatusArea('hello-world', this._indicator);
+    }
+
+    disable() {
+        this._indicator.destroy();
+        this._indicator = null;
+    }
+}
+
+function init() {
+    return new Extension();
+}
