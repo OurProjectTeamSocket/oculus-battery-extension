1223fa7faf612b04932d13f0a93546c3cb8fafbc
NintyS <emil.panecki@gmail.com>
2023-12-21 15:53:21 +0100

    FIX: Added prefix to easily find extension logs

--- extension.js
+++ extension.js
@@ -4,17 +4,19 @@ const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const Gio = imports.gi.Gio;
 
+var prefix = '[OBX]'
+
 var HelloWorldButton = GObject.registerClass(
 class HelloWorldButton extends PanelMenu.Button {
     _init() {
         super._init(0.0, 'Hello World');
 
         let icon = new St.Icon({
-            gicon: Gio.icon_new_for_string("/home/nintys/.local/share/gnome-shell/extensions/test@myextensions.example.com/Images/sonic.png"), // WORK ONLY IF I GIVE A FULL PATH
+            gicon: Gio.icon_new_for_string('/home/nintys/.local/share/gnome-shell/extensions/test@myextensions.example.com/Images/sonic.png'), // WORK ONLY IF I GIVE A FULL PATH
             style_class: 'system-status-icon'
         });        
 
-        log("\033[91m Ikona: ", icon, "\033[0m") // This adds logs - se se logs use this: journalctl -f -o cat /usr/bin/gnome-shell and restart gnome // color text don't work
+        log(prefix, " Ikona: ", icon) // This adds logs - se se logs use this: journalctl -f -o cat /usr/bin/gnome-shell and restart gnome // color text don't work
 
         this.add_child(icon);
     }
