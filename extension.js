489ae3c82b9ffbc333f675a035f5fe175ea21e61
NintyS <emil.panecki@gmail.com>
2023-12-25 03:33:41 +0100

    UPDATE: This code now works as it should, but this code needs a clearing and in next phase of that extensions we need to edit timeout_add with notify to update time of waiting instanty, not in new iteration

--- extension.js
+++ extension.js
@@ -50,9 +50,10 @@ class HelloWorldButton extends PanelMenu.Button {
         log(`${prefix} Ikona: ${icon}`) // This adds logs - se se logs use this: journalctl -f -o cat /usr/bin/gnome-shell and restart gnome // color text don't work
 
         this.connect('button_press_event', () => { // Button updated problem with icon. and a log with captial L
-            log(prefix, `Show IP: ${settings.get_string('ip')}, show time: ${settings.get_string('time')}`)
+            log(prefix, `Show IP: ${settings.get_string('ip')}, show time: ${settings.get_int('time')}`)
         });
 
+        //Test of adding own functions and imports - works <3
         try {
             let funcs = new Functions(settings);
             funcs.Test();
@@ -75,22 +76,45 @@ class Extension {
 
         // TODO: Add a reset if time was changed in options
 
-        this.intervalID = GLib.timeout_add(GLib.PRIORITY_DEFAULT, minutesToMiliseconds(settings.get_int('time')), () => { // here I'm retarded and I tired to get a int with string method
+        try {
 
-            log(prefix, "Loop works")
+            var initialTime = settings.get_int('time');
 
-        //     // TODO: Add a checking if oculus quest is sill connecting IF NOT then try to reccoenct 5 times IF THIS FAILS (IDK, maybe) stop looking and wait for refresh ( to this we need to add buton )
-            if((settings.get_string('ip') == null)||(settings.get_string('ip') == "")) {
-                log(prefix, "FATAL ERROR: ip is null or blank")
-            }
-        //     if(!useCommand(`adb -s ${settings.get_string('ip')}:5555 shell dumpsys battery | grep -E "level|AC|USB"`)) {
-        //         // TODO: Set a error icon ( for now can be any icon )
-        //     } else {
-        //         // TODO: Check the power level, and type of chargeing and choose a good one.
-        //         // TODO: Check if you can set a diffrent colors for icon ( in top bar ) IF YES THEN we don't have to set other icons for other power levels we can just do a grandient from (RGB) 0,255,0 ( top level ) to 255,0,0 ( dead battery )
-        //     }
-            return true; // Do function after X minutes
-        });
+            this.intervalID = GLib.timeout_add(GLib.PRIORITY_DEFAULT, minutesToMiliseconds(settings.get_int('time')), () => { // here I'm retarded and I tired to get a int with string method
+                bodyOfLoop();
+                log(prefix, -1);
+                let currentTime = settings.get_int('time');
+                log(prefix, 0);
+                if (currentTime !== initialTime) {
+                    log(prefix, 1);
+                    log(prefix, "Time has changed from " + initialTime + " to " + currentTime);
+                    if (this.intervalID) {
+                        GLib.source_remove(this.intervalID);
+                        this.intervalID = null;
+                    }
+
+                    log(prefix, 2);
+
+                    initialTime = currentTime;
+
+                    log(prefix, 3);
+
+                    this.intervalID = GLib.timeout_add(GLib.PRIORITY_DEFAULT, minutesToMiliseconds(currentTime), () => {
+                        bodyOfLoop();
+                        return true;
+                    });
+                    
+                }
+                
+                log(prefix, 4);
+
+                return true;
+            });
+        
+        } catch (error) {
+            log(`${prefix}: ${error}`)
+        }
+
     }
 
     disable() {
@@ -120,9 +144,41 @@ useCommand = (commandInput) => {
 }
 
 minutesToMiliseconds = (minutes) => {
-    if(minutes == null) {
+    if(!Number.isInteger(minutes)) {
         log(prefix, "FATAL ERROR: time is null")
         return 1
+    } else {
+        return ( minutes * 60 ) * 1000
     }
-    return ( minutes * 60 ) * 1000
-} 
+}
+
+function bodyOfLoop() {
+
+    var date = new Date();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    date.setHours(hour, minutes, seconds);
+
+    log(prefix, "Loop works", settings.get_int('time'), "time:", date.toLocaleTimeString())
+
+        //     // TODO: Add a checking if oculus quest is sill connecting IF NOT then try to reccoenct 5 times IF THIS FAILS (IDK, maybe) stop looking and wait for refresh ( to this we need to add buton )
+    if((settings.get_string('ip') == null)||(settings.get_string('ip') == "")) {
+        log(prefix, "FATAL ERROR: ip is null or blank")
+    }
+        //     if(!useCommand(`adb -s ${settings.get_string('ip')}:5555 shell dumpsys battery | grep -E "level|AC|USB"`)) {
+        //         // TODO: Set a error icon ( for now can be any icon )
+        //     } else {
+        //         // TODO: Check the power level, and type of chargeing and choose a good one.
+        //         // TODO: Check if you can set a diffrent colors for icon ( in top bar ) IF YES THEN we don't have to set other icons for other power levels we can just do a grandient from (RGB) 0,255,0 ( top level ) to 255,0,0 ( dead battery )
+        //     }
+}
+
+// function DebugLog(Text) {
+//     var date = new Date();
+//     var hour = date.getHours();
+//     var minutes = date.getMinutes();
+//     var seconds = date.getSeconds();
+//     date.setHours(hour, minutes, seconds);
+//     log()
+// }
